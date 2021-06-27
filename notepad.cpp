ce4103f13f497313d8e8868bb34edad237bb8127
R Aravind <aravind.ar786@gmail.com>
2021-06-27 18:07:56 +0530

    feat: added action save as

--- notepad.cpp
+++ notepad.cpp
@@ -31,11 +31,32 @@ void Notepad::on_actionOpen_triggered()
 
     if(!file.open(QIODevice::ReadOnly | QFile::Text)) {
         QMessageBox::warning(this, "Warning", "Cannot display file contents : " + file.errorString());
+        return;
     }
+
     setWindowTitle(filename);
     QTextStream in(&file);
     QString text = in.readAll();
     ui->textEdit->setText(text);
+    file.close();
+}
+
+
+void Notepad::on_actionSave_As_triggered()
+{
+    QString filename = QFileDialog::getSaveFileName(this, "Save as");
+    QFile file(filename);
+
+    if(!file.open(QIODevice::WriteOnly | QFile::Text)) {
+        QMessageBox::warning(this, "Warning", "Cannot save file contents : " + file.errorString());
+        return;
+    }
+
+    currentFile = filename;
+    setWindowTitle(filename);
+    QTextStream out(&file);
+    QString text = ui->textEdit->toPlainText();
+    out << text;
     file.close();
 }
 
